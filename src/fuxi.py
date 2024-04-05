bbf05fbcc4d0cccf9d66621f4d7891de6c1b86ea
Philipp Grill <philipp.grill@freenet.de>
2024-04-05 14:31:05 +0200

    update SpaceTimeCubeEmbedding

--- src/fuxi.py
+++ src/fuxi.py
@@ -1,4 +1,5 @@
 import torch
+from torch import nn
 
 
 class FuXi(torch.nn.Module):
@@ -18,22 +19,22 @@ class FuXi(torch.nn.Module):
         self.layers.append()
 
 
-class CubeEmbedding(torch.nn.Module):
-    def __init__(self, channels):
-        super().__init__()
-        self.layers = torch.nn.ModuleList([
-            torch.nn.Conv3d(
-                in_channels=2,
-                out_channels=channels,
-                kernel_size=(2, 4, 4),
-                stride=(2, 4, 4),
-                padding=0,
-                dilation=1,
-            ),
-            torch.nn.LayerNorm(channels)
-        ])
+class SpaceTimeCubeEmbedding(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(SpaceTimeCubeEmbedding, self).__init__()
+        # 3D Convolution layer with kernel size and stride of (2, 4, 4)
+        # This will reduce the temporal dimension by a factor of 2 and spatial dimensions by a factor of 4
+        self.conv3d = nn.Conv3d(in_channels, out_channels, kernel_size=(2, 4, 4), stride=(2, 4, 4))
+        # Layer Normalization
+        self.layer_norm = nn.LayerNorm(out_channels)
 
     def forward(self, x):
-        for layer in self.layers:
-            x = layer(x)
-        return x
+        # Applying 3D convolution
+        x = self.conv3d(x)
+        # Permuting the dimensions to apply LayerNorm correctly
+        x_permuted = x.permute(1, 2, 3 ,0)  # Move the channel dimension to the end for LayerNorm
+        # Applying layer normalization
+        x_normalized = self.layer_norm(x_permuted)
+        # Permuting back to the original dimension order
+        x_out = x_normalized.permute(3, 0, 1, 2)
+        return x_out
