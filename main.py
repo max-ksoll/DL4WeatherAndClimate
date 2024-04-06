5badf8e778590dce85fe1f00ceff8eee73505cf8
Philipp Grill <philipp.grill@freenet.de>
2024-04-06 10:25:54 +0200

    remove entity

--- main.py
+++ main.py
@@ -25,9 +25,8 @@ logger = logging.getLogger(__name__)
 
 
 def train():
-    entity = "philippgrill"
-    project = "CaseStudiesOfAIImplementationResults"
-    with wandb.init(project=project, entity=entity) as run:
+    project = "DL4WeatherAndClimate"
+    with wandb.init(project=project) as run:
         logger.info('Using {} device'.format(device))
         logger.info('Creating Model')
         model = FuXi(25, 1024, 36, 121, 240, heads=4)
